f4a15ad3fca7d651f730510f3f7d5efbcf6fb7ee
Kawthar248 <kayy.2408@hotmail.com>
2024-01-16 12:48:09 +0300

    creating auto env installation

--- init.bat
+++ init.bat
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
 conda create -n diabetesprediction python=3.11
-=======
-conda create -n diabetesprediction python=3.11
->>>>>>> 70906f9bd6b06b2fe63dac0f5ab4a2093b1807e3
+conda activate diabetesprediction
+pip install -r requirements.txt 
